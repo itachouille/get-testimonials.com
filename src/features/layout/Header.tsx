517927c4c3085175b3092fa9077105813692bc66
Anthony David <anthony.david34@sfr.fr>
2024-03-17 12:38:37 +0100

    theme provider

--- src/features/layout/Header.tsx
+++ src/features/layout/Header.tsx
@@ -1,22 +1,25 @@
 import Image from "next/image";
 import { LoggedInButton } from "../auth/LoggedInButton";
+import { Layout } from "@/components/layout";
+import { ModeToggle } from "../theme/ModeToggle";
 
 export const Header = async () => {
   return (
     <header className="w-full border-b border-border py-2">
-    <div className="flex items-center gap-4">
+      <Layout className="flex items-center gap-4">
         <div className="flex-1">
-           <Image
-        src="/icon.png"
-        width={32}
-        height={32}
-        alt="get-testimonials.com logo"
-      />   
+          <Image
+            src="/icon.png"
+            width={32}
+            height={32}
+            alt="get-testimonials.com logo"
+          />
         </div>
-        <div>
-          <LoggedInButton />   
+        <div className="flex items-center gap-2">
+          <ModeToggle />
+          <LoggedInButton />
         </div>
-    </div>
+      </Layout>
     </header>
   );
 };
